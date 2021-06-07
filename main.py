714547fe530262f795745e4f6cdd39ff8f04a969
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2021-06-07 17:37:32 -0500

    Expanded the solution tester
    Signed-off-by: Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>

--- main.py
+++ main.py
@@ -17,9 +17,12 @@ for scramble in SAMPLE_SCRAMBLES.split("\n")[:150]:
 
     # Test if cube is solved
     s = cube.cubestring()
-    if not (s[0:9] == 9*s[0] and s[9:18] == 9*s[9] and s[18:27] == 9*s[18] and
-            s[27:36] == 9*s[27] and s[36:45] == 9*s[36] and s[45:54] == 9*s[45]):
-        print("Test #", cubes_tested + 1, "failed.")
+    if s[0:9] == 9*s[0] and s[9:18] == 9*s[9] and s[18:27] == 9*s[18] and \
+            s[27:36] == 9*s[27] and s[36:45] == 9*s[36] and s[45:54] == 9*s[45]:
+        print("Cube #", cubes_tested, "solved.")
+        print(cube.solution_sequence)
+    else:
+        print("Cube #", cubes_tested, "FAILED.")
 
 print(cubes_tested, "tests completed in", round(current_time() - start_time, 3),
       "seconds with an average of", round(sum_moves / cubes_tested, 2), "moves")
