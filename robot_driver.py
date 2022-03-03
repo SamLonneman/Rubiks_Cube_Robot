98e197de72ef3d26c7c712e9cc50d0fecc5c00d7
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2022-03-02 20:28:21 -0600

    Created robot class
    Signed-off-by: Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>

--- robot_driver.py
+++ robot_driver.py
@@ -1,138 +1,27 @@
 from RPi import GPIO
 from time import sleep
-from Motor import Motor
+from Robot import Robot
 from Cube import Cube
 
 # GPIO Setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# Initialize Motors
-motorR1 = Motor(15, 18)
-motorR2 = Motor(23, 24)
-motorL1 = Motor(25, 8)
-motorL2 = Motor(7, 1)
-motorF1 = Motor(3, 4)
-motorF2 = Motor(17, 27)
-motorB1 = Motor(10, 9)
-motorB2 = Motor(0, 5)
+# Initialize Robot
+robot = Robot()
 
-# Grab cube
-motorR2.turn(False)
-motorF2.turn(False)
-motorL2.turn(False)
-motorB2.turn(False)
+# Lets GOOOOO
+robot.grab()
 sleep(2000)
 
-# Read in cube state (For now a random scramble)
-cube = Cube("ryowyobbrgbrgrgwbyyybrgwboywgyoorgyoggwwbwwbboyrrwroog")
+# # Read in cube state (For now a random scramble)
+# cube = Cube("ryowyobbrgbrgrgwbyyybrgwboywgyoorgyoggwwbwwbboyrrwroog")
+#
+# # Generate solution
+# cube.solve()
+#
+# # Solve cube
+# for turn in cube.solution_sequence.split():
+#     getattr(robot, turn)()
 
-# Generate solution
-cube.solve()
-
-# Solve cube
-for turn in cube.solution_sequence.split():
-    eval(turn + "()")
-
-# Drop cube
-motorR2.turn()
-motorF2.turn()
-motorL2.turn()
-motorB2.turn()
-
-
-# Robot Functions
-def R():
-    motorR1.turn()
-    motorR2.turn()
-    motorR1.turn(False)
-    motorR2.turn(False)
-
-
-def Ri():
-    motorR1.turn(False)
-    motorR2.turn()
-    motorR1.turn()
-    motorR2.turn(False)
-
-
-def R2():
-    motorR1.turn(True, None, True, 100)
-
-
-def L():
-    motorL1.turn()
-    motorL2.turn()
-    motorL1.turn(False)
-    motorL2.turn(False)
-
-
-def Li():
-    motorL1.turn(False)
-    motorL2.turn()
-    motorL1.turn()
-    motorL2.turn(False)
-
-
-def L2():
-    motorL1.turn(True, None, True, 100)
-
-
-def F():
-    motorF1.turn()
-    motorF2.turn()
-    motorF1.turn(False)
-    motorF2.turn(False)
-
-
-def Fi():
-    motorF1.turn(False)
-    motorF2.turn()
-    motorF1.turn()
-    motorF2.turn(False)
-
-
-def F2():
-    motorF1.turn(True, None, True, 100)
-
-
-def B():
-    motorB1.turn()
-    motorB2.turn()
-    motorB1.turn(False)
-    motorB2.turn(False)
-
-
-def Bi():
-    motorB1.turn(False)
-    motorB2.turn()
-    motorB1.turn()
-    motorB2.turn(False)
-
-
-def B2():
-    motorB1.turn(True, None, True, 100)
-
-
-def x():
-    motorR2.turn()
-    motorR1.turn(False)
-    motorR2.turn(False)
-    motorF2.turn(True, motorB2, True)
-    motorR1.turn(True, motorL1, False)
-    motorL2.turn()
-    motorL1.turn()
-    motorL2.turn(False)
-    motorF2.turn(False, motorB2, False)
-
-
-def xi():
-    motorR2.turn()
-    motorR1.turn(False)
-    motorR2.turn(False)
-    motorF2.turn(True, motorB2, True)
-    motorR1.turn(False, motorL1, True)
-    motorL2.turn()
-    motorL1.turn()
-    motorL2.turn(False)
-    motorF2.turn(False, motorB2, False)
+robot.drop()
---
+++ venv/Robot.py
@@ -0,0 +1,107 @@
+from Motor import Motor
+
+class Robot:
+
+    def __init__(self):
+        self.motorR1 = Motor(15, 18)
+        self.motorR2 = Motor(23, 24)
+        self.motorL1 = Motor(25, 8)
+        self.motorL2 = Motor(7, 1)
+        self.motorF1 = Motor(3, 4)
+        self.motorF2 = Motor(17, 27)
+        self.motorB1 = Motor(10, 9)
+        self.motorB2 = Motor(0, 5)
+
+    def grab(self):
+        self.motorR2.turn(False)
+        self.motorF2.turn(False)
+        self.motorL2.turn(False)
+        self.motorB2.turn(False)
+
+    def drop(self):
+        self.motorR2.turn()
+        self.motorF2.turn()
+        self.motorL2.turn()
+        self.motorB2.turn()
+
+    def R(self):
+        self.motorR1.turn()
+        self.motorR2.turn()
+        self.motorR1.turn(False)
+        self.motorR2.turn(False)
+
+    def Ri(self):
+        self.motorR1.turn(False)
+        self.motorR2.turn()
+        self.motorR1.turn()
+        self.motorR2.turn(False)
+
+    def R2(self):
+        self.motorR1.turn(True, None, True, 100)
+
+    def L(self):
+        self.motorL1.turn()
+        self.motorL2.turn()
+        self.motorL1.turn(False)
+        self.motorL2.turn(False)
+
+    def Li(self):
+        self.motorL1.turn(False)
+        self.motorL2.turn()
+        self.motorL1.turn()
+        self.motorL2.turn(False)
+
+    def L2(self):
+        self.motorL1.turn(True, None, True, 100)
+
+    def F(self):
+        self.motorF1.turn()
+        self.motorF2.turn()
+        self.motorF1.turn(False)
+        self.motorF2.turn(False)
+
+    def Fi(self):
+        self.motorF1.turn(False)
+        self.motorF2.turn()
+        self.motorF1.turn()
+        self.motorF2.turn(False)
+
+    def F2(self):
+        self.motorF1.turn(True, None, True, 100)
+
+    def B(self):
+        self.motorB1.turn()
+        self.motorB2.turn()
+        self.motorB1.turn(False)
+        self.motorB2.turn(False)
+
+    def Bi(self):
+        self.motorB1.turn(False)
+        self.motorB2.turn()
+        self.motorB1.turn()
+        self.motorB2.turn(False)
+
+    def B2(self):
+        self.motorB1.turn(True, None, True, 100)
+
+    def x(self):
+        self.motorR2.turn()
+        self.motorR1.turn(False)
+        self.motorR2.turn(False)
+        self.motorF2.turn(True, motorB2, True)
+        self.motorR1.turn(True, motorL1, False)
+        self.motorL2.turn()
+        self.motorL1.turn()
+        self.motorL2.turn(False)
+        self.motorF2.turn(False, motorB2, False)
+
+    def xi(self):
+        self.motorR2.turn()
+        self.motorR1.turn(False)
+        self.motorR2.turn(False)
+        self.motorF2.turn(True, motorB2, True)
+        self.motorR1.turn(False, motorL1, True)
+        self.motorL2.turn()
+        self.motorL1.turn()
+        self.motorL2.turn(False)
+        self.motorF2.turn(False, motorB2, False)
