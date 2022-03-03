d02df2a363c3340572adf66601604388094c2f28
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2022-03-02 20:37:54 -0600

    Moved Robot.py
    Signed-off-by: Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>

---
+++ Robot.py
@@ -0,0 +1,31 @@
+from Cube import Cube
+from Helpers import SOLVED, SAMPLE_SCRAMBLES
+from time import time as current_time
+
+# SEQUENCE VALIDITY CHECKER
+start_time = current_time()
+sum_moves = 0
+sum_rotations = 0
+cubes_tested = 0
+for scramble in SAMPLE_SCRAMBLES.split("\n")[:150]:
+    cube = Cube(SOLVED)
+    cube.move(scramble, False)
+    cube.solve()
+    cubes_tested += 1
+    sum_moves += len(cube.solution_sequence.split())
+    sum_rotations += cube.solution_sequence.count('x')
+
+    # Check if solution sequence is valid (Remove if testing half turn metric)
+    cube2 = Cube(SOLVED)
+    cube2.move(scramble, False)
+    cube2.move(cube.solution_sequence)
+    s = cube2.cubestring()
+    if s[0:9] == 9*s[0] and s[9:18] == 9*s[9] and s[18:27] == 9*s[18] and s[27:36] == 9*s[27] and s[36:45] == 9*s[36] and s[45:54] == 9*s[45]:
+        pass  # print("Cube #", cubes_tested, "solved.")
+    else:
+        print("Cube #", cubes_tested, "FAILED.")
+
+# Print results
+print(cubes_tested, "tests completed in", round(current_time() - start_time, 3),
+      "seconds with an average of", round(sum_moves / cubes_tested, 2), "moves, of which",
+      round(sum_rotations / cubes_tested, 2), "were rotations.")
