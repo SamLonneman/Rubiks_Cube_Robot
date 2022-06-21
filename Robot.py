d8241874bc0abf838b2b7b1b3ea8561bc3076e5a
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2022-06-20 23:14:48 -0500

    Added camera functionality
    Signed-off-by: Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>

--- Robot.py
+++ Robot.py
@@ -1,6 +1,11 @@
-from RPi import GPIO
-from Motor import Motor
+from io import BytesIO
 from os import system
+
+from PIL import Image
+from RPi import GPIO
+from picamera import PiCamera
+
+from Motor import Motor
 
 
 class Robot:
@@ -29,6 +34,85 @@ class Robot:
         self.motorF2 = Motor(11, 9)
         self.motorB1 = Motor(10, 22)
         self.motorB2 = Motor(5, 0)
+        self.camera = PiCamera()
+        self.yellow = (117, 142, 0)
+        self.red = (132, 17, 14)
+        self.green = (0, 109, 2)
+        self.orange = (178, 70, 8)
+        self.blue = (0, 31, 53)
+        self.white = (156, 151, 109)
+        self.PIXELS = [
+            (951, 720),
+            (887, 571),
+            (755, 495),
+            (1042, 586),
+            (967, 511),
+            (902, 433),
+            (1177, 519),
+            (1053, 444),
+            (979, 303),
+        ]
+
+    def rgb_diff(self, rgb1, rgb2):
+        difference = 0
+        for i in range(3):
+            difference += abs(rgb1[i] - rgb2[i])
+        return difference
+
+    def take_picture(self):
+        stream = BytesIO()
+        self.camera.capture(stream, format='jpeg')
+        return Image.open(stream).load()
+
+    def calibrate(self):
+        self.yellow = self.take_picture()[self.PIXELS[4]]
+        self.z()
+        self.red = self.take_picture()[self.PIXELS[4]]
+        self.x()
+        self.green = self.take_picture()[self.PIXELS[4]]
+        self.x()
+        self.orange = self.take_picture()[self.PIXELS[4]]
+        self.x()
+        self.blue = self.take_picture()[self.PIXELS[4]]
+        self.x()
+        self.z()
+        self.white = self.take_picture()[self.PIXELS[4]]
+        self.z2()
+
+    def pick_color(self, rgb):
+        differences = {
+            self.rgb_diff(rgb, self.yellow): 'y',
+            self.rgb_diff(rgb, self.red): 'r',
+            self.rgb_diff(rgb, self.green): 'g',
+            self.rgb_diff(rgb, self.orange): 'o',
+            self.rgb_diff(rgb, self.blue): 'b',
+            self.rgb_diff(rgb, self.white): 'w',
+        }
+        return differences[min(differences.keys())]
+
+    def get_cubestring_segment(self, order):
+        cubestring = str()
+        image = self.take_picture()
+        for i in order:
+            cubestring += self.pick_color(image[self.PIXELS[i]])
+        return cubestring
+
+    def read_cube(self):
+        cubestring = str()
+        cubestring += self.get_cubestring_segment((0, 1, 2, 3, 4, 5, 6, 7, 8))
+        self.z()
+        cubestring += self.get_cubestring_segment((2, 5, 8, 1, 4, 7, 0, 3, 6))
+        self.x()
+        cubestring += self.get_cubestring_segment((2, 5, 8, 1, 4, 7, 0, 3, 6))
+        self.x()
+        cubestring += self.get_cubestring_segment((2, 5, 8, 1, 4, 7, 0, 3, 6))
+        self.x()
+        cubestring += self.get_cubestring_segment((2, 5, 8, 1, 4, 7, 0, 3, 6))
+        self.x()
+        self.z()
+        cubestring += self.get_cubestring_segment((6, 3, 0, 7, 4, 1, 8, 5, 2))
+        self.z2()
+        return cubestring
 
     def drop(self):
         self.motorR2.retract(self.motorL2)
