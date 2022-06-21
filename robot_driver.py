c77b055735f71ee8080891a1a11a0ada087228fd
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2022-06-20 21:44:13 -0500

    Added interface button logic
    Signed-off-by: Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>

--- robot_driver.py
+++ robot_driver.py
@@ -1,32 +1,33 @@
+from Helpers import SOLVED
 from Robot import Robot
 from Cube import Cube
+from time import sleep
+from RPi import GPIO
+from os import system
 
 # Initialize Robot
 robot = Robot()
 
-# Lets GOOOOO
-for _ in range(1):
-    robot.R()
-    robot.L()
-    robot.F()
-    robot.B()
-    robot.x()
-    robot.xi()
-    robot.Bi()
-    robot.Fi()
-    robot.Li()
-    robot.Ri()
+while not GPIO.input(robot.shutdown_button):
 
+    # Wait for input
+    while not (GPIO.input(robot.shutdown_button) or GPIO.input(robot.solve_button)):
+        sleep(0.1)
 
-# # Read in cube state (For now a random scramble)
-# cube = Cube("ryowyobbrgbrgrgwbyyybrgwboywgyoorgyoggwwbwwbboyrrwroog")
-#
-# # Generate solution
-# cube.solve()
-#
-# # Solve cube
-# for turn in cube.solution_sequence.split():
-#     getattr(robot, turn)()
-#
-# # Drop cube
-# robot.drop()
+    # If solve button was pressed, solve
+    if not GPIO.input(robot.shutdown_button):
+
+        # Read in cube state (For now a superflip)
+        cube = Cube(SOLVED)
+        cube.move("M M Ui R R Di S M M U Mi U U F F Di S M M Ui R R Ui", False)
+
+        # Solve cube, abort at any time if necessary
+        cube.solve()
+        for turn in cube.solution_sequence.split():
+            if GPIO.input(robot.abort_button):
+                break
+            getattr(robot, turn)()
+
+# Shut down
+GPIO.cleanup()
+system("sudo shutdown -h now")
